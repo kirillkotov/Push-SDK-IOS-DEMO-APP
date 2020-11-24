c2577821722f84a46c3fdfd5ac7d012ff9f5e490
Dzmitry Buynovskiy <imperituro.ard@gmail.com>
2020-11-25 02:40:26 +0300

    integrated sdk 1.0.0.30

--- PushDTest/AppDelegate.swift
+++ PushDTest/AppDelegate.swift
@@ -13,45 +13,41 @@ import PushSDK
 
 @UIApplicationMain
 public class AppDelegate: UIResponder, UIApplicationDelegate {
-    let processor = PushKProcessing.init()
-    let fb_ad = PushKFirebaseSdk.init()
+    let fb_ad = PushSDKFirebase.init()
+    
     public var window: UIWindow?
-    let gcmMessageIDKey = "gcm.message_id"
+    
     @IBOutlet weak var sentNotificationLabel: UILabel!
     
     
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        Messaging.messaging().delegate = self
-        Messaging.messaging().shouldEstablishDirectChannel = true
-        fb_ad.fb_fun0_application(didFinishLaunchingWithOptions: launchOptions)
-        application.registerForRemoteNotifications()
-        fb_ad.registerForPushNotifications()
+        fb_ad.fbInitApplication(didFinishLaunchingWithOptions: launchOptions)
+        //application.registerForRemoteNotifications()
         UNUserNotificationCenter.current().delegate = self
-       
         return true
     }
     
     func application(application: UIApplication,  didReceiveRemoteNotification userInfo: [NSObject : AnyObject],  fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
         
-        fb_ad.fb_fun1_application(didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+        fb_ad.fbInitApplication(didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
     }
     
     public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-        fb_ad.fb_fun2_application(didReceiveRemoteNotification: userInfo)
+        fb_ad.fbInitApplication(didReceiveRemoteNotification: userInfo)
     }
     
     
     public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
                             fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         
-        fb_ad.fb_fun3_application(didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+        fb_ad.fbInitApplication(didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
     }
     
     
     public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        fb_ad.fb_fun4_application(didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+        fb_ad.fbInitApplication(didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
     }
 }
 
@@ -74,74 +70,30 @@ public extension UIViewController
     }
 }
 
-
 extension AppDelegate: UNUserNotificationCenterDelegate{
     
     public func userNotificationCenter(_ center: UNUserNotificationCenter,
                                        willPresent notification: UNNotification,
                                        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        let userInfo = notification.request.content.userInfo
-        
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-        // Print message ID.
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
         completionHandler([.alert, .sound, .badge])
     }
     
     public func userNotificationCenter(_ center: UNUserNotificationCenter,
                                        didReceive response: UNNotificationResponse,
                                        withCompletionHandler completionHandler: @escaping () -> Void) {
-        let userInfo = response.notification.request.content.userInfo
-        // Print message ID.
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
         completionHandler()
     }
 }
-
-
-extension AppDelegate: MessagingDelegate {
-    
-    @IBAction func notify() {
-        fb_ad.fb_notify_messaging()
-    }
-    
-    public func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
-        fb_ad.fb_remote_messaging(remoteMessage: remoteMessage.appData as NSDictionary)
-    }
-    
-    public func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
-        fb_ad.fb_token_messaging(didReceiveRegistrationToken: fcmToken )
-    }
-}
-
 
 extension ViewController: UNUserNotificationCenterDelegate {
     
     //for displaying notification when app is in foreground
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        //If you don't want to show notification when app is open, do something here else and make a return here.
-        //Even you you don't implement this delegate method, you will not see the notification on the specified controller. So, you have to implement this delegate and make sure the below line execute. i.e. completionHandler.
-        
         completionHandler([.alert, .badge, .sound])
     }
     
     // For handling tap and user actions
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        
-        switch response.actionIdentifier {
-        case "action1":
-            print("Action First Tapped")
-        case "action2":
-            print("Action Second Tapped")
-        default:
-            break
-        }
         completionHandler()
     }
 }
--- PushDTest/ViewController.swift
+++ PushDTest/ViewController.swift
@@ -8,31 +8,34 @@ import PushSDK
 import SwiftyBeaver
 import UserNotifications
 
+
 class ViewController: UIViewController {
     
     
-    let push_adapter_sdk = PushSDK.init(platform_branch: PushSdkParametersPublic.branchMasterValue, log_level: SwiftyBeaver.Level.debug, basePushURL: "https://example.com/api/")
+    let pushAdapterSdk = PushSDK.init(platform_branch: PushSDKVar.branchMasterValue, log_level: PushSDKVar.LOGLEVEL_DEBUG, basePushURL: "https://test-push.hyber.im/api/")
+    
+    //for production
+    //let pushAdapterSdk = PushSDK.init(basePushURL: "https://push.hyber.im/api/")
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(onDidReceiveData(_:)), name: .didReceiveData, object: nil)
+        
+        //register in notification center
+        NotificationCenter.default.addObserver(self, selector: #selector(onReceiveFromPushServer(_:)), name: .receivePushKData, object: nil)
         UNUserNotificationCenter.current().delegate = self
     
     }
     
    
     //processing incoming data message
-    @objc func onDidReceiveData(_ notification:Notification) {
+    @objc func onReceiveFromPushServer(_ notification: Notification) {
         // Do something now
-        print("ncenter shshdfghf")
-        print(notification.userInfo)
-        let jsonData = try? JSONSerialization.data(withJSONObject: notification.userInfo, options: [])
-        let jsonString = String(data: jsonData!, encoding: .utf8)!
-        let newString = String(jsonString).replacingOccurrences(of: "\\", with: "", options: .literal, range: nil)
-        print(newString)
-        textOutput.text = textOutput.text + "\n" + newString
+        let incomMessage = PushSDK.parseIncomingPush(message: notification).messageFir
+        print(incomMessage.message.toString())
+        textOutput.text = textOutput.text + "\n" + incomMessage.message.toString()
     }
+    
     
     @IBOutlet weak var textOutput: UITextView!
     @IBOutlet weak var txt1fiIn: UITextField!
@@ -45,13 +48,13 @@ class ViewController: UIViewController {
     
     //test button, register this device on push server
     @IBAction func button2register(_ sender: UIButton) {
-        let tttt: PushKFunAnswerRegister = push_adapter_sdk.push_register_new(user_phone: txt1fiIn.text ?? "375291234567", user_password: "1", x_push_sesion_id: PushKConstants.firebase_registration_token ?? "", x_push_ios_bundle_id: "12345678", X_Push_Client_API_Key: "test")
+        let tttt: PushKFunAnswerRegister = pushAdapterSdk.push_register_new(user_phone: txt1fiIn.text ?? "375291234567", user_password: "1", x_push_sesion_id: PushKConstants.firebase_registration_token ?? "", x_push_ios_bundle_id: "12345678", X_Push_Client_API_Key: "test")
         textOutput.text = "\(String(textOutput.text))\n\(tttt.toString())"
     }
     
     //test button, check message queue on server
     @IBAction func button3core(_ sender: UIButton) {
-        let queue = push_adapter_sdk.push_check_queue()
+        let queue = pushAdapterSdk.push_check_queue()
         textOutput.text = "\(String(textOutput.text))\n\(queue.toString())"
     }
     
@@ -62,40 +65,41 @@ class ViewController: UIViewController {
     
     //clear only local deviceid
     @IBAction func button5clearself(_ sender: UIButton) {
-        textOutput.text = "\(String(textOutput.text))\n\(push_adapter_sdk.push_clear_current_device().toString())"
+        textOutput.text = "\(String(textOutput.text))\n\(pushAdapterSdk.push_clear_current_device().toString())"
     }
     
     //get all devices from server registered for current msisdn
     @IBAction func button6getdevice(_ sender: UIButton) {
-        textOutput.text = "\(String(textOutput.text))\n\(push_adapter_sdk.push_get_device_all_from_server().toString())"
+        textOutput.text = "\(String(textOutput.text))\n\(pushAdapterSdk.push_get_device_all_from_server().toString())"
     }
     
     //send test callback message to specific message
     @IBAction func button7callback(_ sender: UIButton) {
-        textOutput.text = "\(String(textOutput.text))\n\(push_adapter_sdk.push_send_message_callback(message_id: "test", message_text: "privet").toString())"
+        textOutput.text = "\(String(textOutput.text))\n\(pushAdapterSdk.push_send_message_callback(message_id: "test", message_text: "privet").toString())"
     }
     
     
     //send delivery report for test with fake message id
     @IBAction func button8dr(_ sender: UIButton) {
-        textOutput.text = "\(String(textOutput.text))\n\(push_adapter_sdk.push_message_delivery_report(message_id: "1251fqf4").toString())"
+        textOutput.text = "\(String(textOutput.text))\n\(pushAdapterSdk.push_message_delivery_report(message_id: "1251fqf4").toString())"
     }
     
     //token update on server test button
     @IBAction func button9update(_ sender: UIButton) {
-        textOutput.text = "\(String(textOutput.text))\n\(push_adapter_sdk.push_update_registration().toString())"
+        textOutput.text = "\(String(textOutput.text))\n\(pushAdapterSdk.push_update_registration().toString())"
     }
     
     //get message history test button
     @IBAction func button10gethistory(_ sender: UIButton) {
-        let hist: PushKFunAnswerGetMessageHistory = push_adapter_sdk.push_get_message_history(period_in_seconds: 12345)
+        let hist: PushKFunAnswerGetMessageHistory = pushAdapterSdk.push_get_message_history(period_in_seconds: 12345)
         textOutput.text = "\(String(textOutput.text))\n\(hist.toString())"
     }
     
     //clear all registered devices test button
     @IBAction func button11clearall(_ sender: Any) {
-        let cle_p = push_adapter_sdk.push_clear_all_device()
+        let cle_p = pushAdapterSdk.push_clear_all_device()
         textOutput.text = "\(String(textOutput.text))\n\(cle_p.toString())"
     }
 }
+
 
